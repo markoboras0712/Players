5908bb9093700867c2f3b3ef44abb88d8283cf84
markoboras <mboras1@etfos.hr>
2022-02-11 15:21:06 +0100

    eslint and prettier added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "core-js/features/array/flat-map";
-import "core-js/features/map";
-import "core-js/features/promise";
-import "core-js/features/set";
-import "raf/polyfill";
-import "whatwg-fetch";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'core-js/features/array/flat-map';
+import 'core-js/features/map';
+import 'core-js/features/promise';
+import 'core-js/features/set';
+import 'raf/polyfill';
+import 'whatwg-fetch';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("app-root"));
+ReactDOM.render(<App />, document.getElementById('app-root'));
