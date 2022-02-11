59f4ff800798e973ea53fd27d17aa83606f9f38f
markoboras <mboras1@etfos.hr>
2022-02-11 15:02:54 +0100

    tsconfig noEmit setted to false, App.tsx imported in index.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./Counter";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Create React App from Scratch Using Typescript..!!</h1>
+      <Counter />
+    </div>
+  );
+};
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -7,5 +7,6 @@ import "core-js/features/promise";
 import "core-js/features/set";
 import "raf/polyfill";
 import "whatwg-fetch";
+import App from "./App";
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById("app-root"));
+ReactDOM.render(<App />, document.getElementById("app-root"));
